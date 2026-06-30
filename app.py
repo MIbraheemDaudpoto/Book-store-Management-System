1d85592615b1dbcdcbb27a8411190e4b0d6e20e2
MIbraheemDaudpoto <codewithibf@gmail.com>
2026-06-30 22:14:18 +0500

    Save local bookstore progress

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask, redirect,request,render_template
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-@app.route('/')
-def Home():
-    return render_template('index.html')
+# Database Configuration
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///BooksDataBase.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 
-if __name__== "__main__":
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(100), unique=True, nullable=False)
+
+    author = db.Column(db.String(100), nullable=False)
+
+    category = db.Column(db.String(100), nullable=False)
+
+    price = db.Column(db.Integer, nullable=False)
+
+    quantity = db.Column(db.Integer, nullable=False)
+
+
+# Create Database
+with app.app_context():
+    db.create_all()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
